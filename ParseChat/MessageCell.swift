a79d23e55cc14a22cdafc3d96f5ad8f847451999
swappy208@gmail.com <swappy208@gmail.com>
2017-02-25 01:44:43 -0500

    Updated the project and added gif

---
+++ ParseChat/MessageCell.swift
@@ -0,0 +1,14 @@
+//
+//  messageCell.swift
+//  ParseChat
+//
+//  Created by Swapnil Tamrakar on 2/24/17.
+//  Copyright © 2017 Swapnil Tamrakar. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+}
